34f45949d48a8ed1ddc2da8519b791bd77510822
Michael Hyung Heo <sunemo83@gmail.com>
2017-10-08 00:07:37 +0900

    Update sw.js

--- pay/sw.js
+++ pay/sw.js
@@ -15,3 +15,17 @@ self.addEventListener('install', (event) => {
       })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
