32fb14236eaed5342212a143123b6ae1497d9a63
Michael Hyung Heo <sunemo83@gmail.com>
2017-10-08 20:24:55 +0900

    Update sw.js

--- pay/sw.js
+++ pay/sw.js
@@ -8,44 +8,38 @@ self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] install event');
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
         return cache.addAll(urlsToCache);
-      })
+    })
   );
-  event.waitUntil(self.registration.paymentManager.paymentInstruments.set({
-    instrumentKey,
+  event.waitUntil(self.registration.paymentManager.paymentInstruments.set(
     {
-      name: "HULPAN_PAY",
-      enabledMethods: ["https://hulpan.com/"],
-      icons: [{
-        src: "icon/lowres.webp",
-        sizes: "48x48",
-        type: "image/webp"
-      }]
-    }
-  });
-);
+      instrumentKey,
+      {
+        name: "HULPAN_PAY",
+        enabledMethods: ["https://hulpan.com/"],
+        icons: [{
+          src: "icon/lowres.webp",
+          sizes: "48x48",
+          type: "image/webp"
+        }]
+      }
+    })
+  );
+});  
 
 self.addEventListener('fetch', (event) => {
   console.log('[ServiceWorker] fetch event');
-  self.registration.showNotification('ServiceWorker Fetch', {
-    actions: [{action: 'get', title: '[HUL] ServiceWorker Fetch'}]
-  });
-  var result = self.registration.paymentManager;
-  console.log("[ServiceWorker] " + JSON.stringify(result));
-  self.registration.showNotification(result, {
-    actions: [{action: 'get', title: '[HUL]' + result}]
-  });
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then((response) => {
+      self.registration.showNotification('ServiceWorker Fetch', {
+        actions: [{action: 'get', title: '[HUL] ServiceWorker Fetch'}]
+      });
+      if (response) {
+        return response;
       }
-    )
-  );
+      return fetch(event.request);
+    }
+  )
+);
 });
